airbornio/firetext

commit 2a7cc480cae95673f5a9532f4ad561f8f9e1fc02

Revamp Share UI

scripts/shared.js
@@ -9,13 +9,15 @@ firetext.shared = {
 		});
 	},
 	get: function(path) {
-		return JSON.parse(localStorage['firetext.shared'] || '{}')[path];
+		return JSON.parse(localStorage['firetext.shared'] || '{}')[path] || {};
 	},
 	getOptions: function(path, callback) {
 		var attrs = this.get(path);
-		if(attrs) {
+		var collab = attrs['collab-ACL'] && attrs['collab-ACL'] !== 'private';
+		var publish = attrs['publish-ACL'] && attrs['publish-ACL'] !== 'private';
+		if(collab || publish) {
 			ljs.load('sjcl', function() {
-				if(attrs['publish-ACL'] && attrs['publish-ACL'] !== 'private') {
+				if(publish) {
 					var iter = attrs['publish-use-password'] ? attrs['publish-iter'] : 101;
 					var password = attrs['publish-use-password'] ? attrs['publish-password'] : '';
 					var salt = attrs['publish-salt'];
@@ -28,7 +30,7 @@ firetext.shared = {
 				var private_key = key.slice(0, 128/32); // First half
 				var shared_key = key.slice(128/32); // Second half
 				var authkey = sjcl.codec.hex.fromBits(shared_key).toUpperCase();
-				if(attrs['collab-ACL'] && attrs['collab-ACL'] !== 'private') {
+				if(collab) {
 					authkey += ':' + sjcl.codec.hex.fromBits(
 						sjcl.misc.cachedPbkdf2(
 							attrs['collab-use-password'] ? attrs['collab-password'] : '', {
@@ -39,7 +41,7 @@ firetext.shared = {
 					).toUpperCase();
 				}
 				callback({
-					ACL: attrs['collab-ACL'] && attrs['collab-ACL'] !== 'private' ? attrs['collab-ACL'] : attrs['publish-ACL'],
+					ACL: collab ? attrs['collab-ACL'] : attrs['publish-ACL'],
 					S3Prefix: attrs.S3Prefix,
 					object: attrs.object,
 					password: private_key,
@@ -60,7 +62,7 @@ firetext.shared = {
 	},
 	updateCollab: function(path) {
 		var attrs = this.get(path);
-		if(attrs && attrs['collab-ACL'] !== 'private') {
+		if(attrs['collab-ACL'] && attrs['collab-ACL'] !== 'private') {
 			if(!socket) {
 				ljs.load('collab', function() {
 					initSocket();

index.html
@@ -233,13 +233,13 @@
 	</section>
 	<div class="background" data-click="navBack"></div>
 	
-	<section role="dialog" id="collab" class="config-dialog">
+	<section role="dialog" id="share" class="config-dialog">
 		<header>
 			<button data-click="navBack" data-l10n-title="close"><span class="icon icon-close"></span></button>
 			<menu type="toolbar">
-				<button data-click="collab" data-l10n-id="okay"></button>
+				<button data-click="navBack" data-l10n-id="okay"></button>
 			</menu>
-			<h1 data-l10n-id="collaborate"></h1>
+			<h1 data-l10n-id="share"></h1>
 		</header>
 		<div role="main">
 			<div class="form">
@@ -247,65 +247,17 @@
 				<fieldset class="button-block">
 					<table>
 						<tr>
-							<td data-l10n-id="public"></td>
-							<td>
-								<p>
-									<select id="collab-ACL">
-										<option value="private" data-l10n-id="no"></option>
-										<option value="public-read" data-l10n-id="can-view"></option>
-										<option value="public-read-write" data-l10n-id="can-edit"></option>
-									</select>
-								</p>
-							</td>
+							<td><h3 data-l10n-id="publish"></h3></td>
 						</tr>
 						<tr>
-							<td><label><input type="checkbox" id="collab-use-password"> <span data-l10n-id="use-password"></span></label></td>
 							<td>
-								<p>
-									<input type="text" id="collab-password" data-enter="collab">
-									<button type="reset" data-mouse-down="generateNewPasswordForm" class="input-icon-button icon-refresh"></button>
-									<input type="hidden" id="collab-iter">
-									<input type="hidden" id="collab-salt">
-								</p>
+								<button data-click="publish" data-l10n-id="publish-link-generate" id="publish-link-generate"></button>
+								<button data-click="publish" data-l10n-id="publish-link-revoke" id="publish-link-revoke" data-click-revoke></button>
 							</td>
-						</tr>
-						<tr>
-							<td data-l10n-id="link"></td>
 							<td>
 								<p>
-									<input readonly="" type="text" id="collab-link">
-									<button type="reset" data-mouse-down="copyForm" data-clipboard-target="#collab-link" class="copy-button input-icon-button icon-content-copy"></button>
-								</p>
-							</td>
-						</tr>
-					</table>
-				</fieldset>
-			</div>
-		</div>
-	</section>
-	<div class="background" data-click="navBack"></div>
-	
-	<section role="dialog" id="publish" class="config-dialog">
-		<header>
-			<button data-click="navBack" data-l10n-title="close"><span class="icon icon-close"></span></button>
-			<menu type="toolbar">
-				<button data-click="publish" data-l10n-id="okay"></button>
-			</menu>
-			<h1 data-l10n-id="publish"></h1>
-		</header>
-		<div role="main">
-			<div class="form">
-				<br>
-				<fieldset class="button-block">
-					<table>
-						<tr>
-							<td data-l10n-id="public"></td>
-							<td>
-								<p>
-									<select id="publish-ACL">
-										<option value="private" data-l10n-id="no"></option>
-										<option value="public-read" data-l10n-id="can-view"></option>
-									</select>
+									<input readonly="" type="text" id="publish-link">
+									<button type="reset" data-mouse-down="copyForm" data-clipboard-target="#publish-link" class="copy-button input-icon-button icon-content-copy"></button>
 								</p>
 							</td>
 						</tr>
@@ -313,19 +265,43 @@
 							<td><label><input type="checkbox" id="publish-use-password"> <span data-l10n-id="use-password"></span></label></td>
 							<td>
 								<p>
-									<input type="text" id="publish-password" data-enter="publish">
-									<button type="reset" data-mouse-down="generateNewPasswordForm" class="input-icon-button icon-refresh"></button>
+									<input type="text" id="publish-password" class="share-password-input" data-enter="publish">
+									<button type="reset" data-mouse-down="generateNewPasswordForm" class="generate-button input-icon-button icon-autorenew"></button>
+									<button type="reset" data-mouse-down="copyForm" data-clipboard-target="#publish-password" class="copy-button input-icon-button icon-content-copy"></button>
 									<input type="hidden" id="publish-iter">
 									<input type="hidden" id="publish-salt">
 								</p>
 							</td>
 						</tr>
+					</table>
+				</fieldset>
+				<br>
+				<fieldset class="button-block">
+					<table>
 						<tr>
-							<td data-l10n-id="link"></td>
+							<td><h3 data-l10n-id="collaborate"></h3></td>
+						</tr>
+						<tr>
+							<td>
+								<button data-click="collab" data-l10n-id="collab-link-generate" id="collab-link-generate"></button>
+								<button data-click="collab" data-l10n-id="collab-link-revoke" id="collab-link-revoke" data-click-revoke></button>
+							</td>
 							<td>
 								<p>
-									<input readonly="" type="text" id="publish-link">
-									<button type="reset" data-mouse-down="copyForm" data-clipboard-target="#publish-link" class="copy-button input-icon-button icon-content-copy"></button>
+									<input readonly="" type="text" id="collab-link">
+									<button type="reset" data-mouse-down="copyForm" data-clipboard-target="#collab-link" class="copy-button input-icon-button icon-content-copy"></button>
+								</p>
+							</td>
+						</tr>
+						<tr>
+							<td><label><input type="checkbox" id="collab-use-password"> <span data-l10n-id="use-password"></span></label></td>
+							<td>
+								<p>
+									<input type="text" id="collab-password" class="share-password-input" data-enter="collab">
+									<button type="reset" data-mouse-down="generateNewPasswordForm" class="generate-button input-icon-button icon-autorenew"></button>
+									<button type="reset" data-mouse-down="copyForm" data-clipboard-target="#collab-password" class="copy-button input-icon-button icon-content-copy"></button>
+									<input type="hidden" id="collab-iter">
+									<input type="hidden" id="collab-salt">
 								</p>
 							</td>
 						</tr>
@@ -374,8 +350,6 @@
 				<ul>
 					<li><a href="#" data-click="saveFromEditor"><span class="icon icon-content-save"></span><span data-l10n-id="save"></span></a></li>
 					<li><a href="#" data-click="nav" data-click-location="save-as" data-l10n-id="save-as"></a></li>
-					<li><a href="#" data-click="openCollab" data-l10n-id="collaborate"></a></li>
-					<li><a href="#" data-click="openPublish" data-l10n-id="publish"></a></li>
 					<li><a href="#" data-click="downloadFile"><span class="icon icon-cloud-download"></span><span data-l10n-id="export"></span></a></li>
 					<li><a href="#" data-click="openWordCount"><span class="icon icon-counter"></span><span data-l10n-id="word-count"></span></a></li>
 					<li><a href="#" data-click="openPageSetup" data-l10n-id="page-setup"></a></li>
@@ -412,6 +386,7 @@
 			<button id="header-close-file" data-click="closeFile" class="no-zen" data-l10n-title="close"><span class="icon icon-close"></span></button>
 			<menu type="toolbar">
 				<button data-click="fullscreen" id="editor-zen-button" data-l10n-title="fullscreen"><span class="icon icon-fullscreen"></span></button>
+				<button data-click="openShare" data-l10n-title="share"><span class="icon icon-account-multiple"></span></button>
 				<button data-click="openHistory" class="menu drawerEnabled no-zen" id="open-history-button" data-l10n-title="history"><span class="icon icon-history"></span></button>
 				<button data-click="sidebar" data-click-location="sidebar_main" class="menu drawerEnabled no-zen" data-l10n-title="menu"><span class="icon icon-menu"></span></button>
 			</menu>

locales/en.app.properties
@@ -11,10 +11,9 @@ can-edit=Can Edit
 can-view=Can View
 cancel=Cancel
 cant-collab-nohist=You can only collaborate on one of the "Worker" plans.
-cant-collab-noninternal=You can only collaborate on documents on internal storage.
 cant-collab-too-old=Your version of Airborn OS seems to be too old to collaborate on this document. Please update it and try again.
-cant-publish-demo=You can't publish documents from the Demo version.
-cant-publish-noninternal=You can only publish documents on internal storage.
+cant-publish-demo=You can't share a read-only link from the Demo version.
+cant-share-noninternal=You can only share documents on internal storage.
 cant-view-hist-noninternal=You can only view the history of documents on internal storage.
 cant-view-hist=You can only view history on one of the "Worker" plans.
 center=Center
@@ -28,6 +27,8 @@ click-to-open-plans=Click here to open Plans.
 click-to-update=Click here to update Airborn OS.
 close=Close
 cloud=Cloud
+collab-link-generate=Generate Collaboration Link
+collab-link-revoke=Revoke Collaboration Link
 collaborate=Collaborate
 contains-special-characters=Special characters are not allowed. Please revise.
 content-not-defined=The content was not defined
@@ -117,8 +118,9 @@ portrait=Portrait
 print-view=Print View
 print=Print
 privacy=Privacy
-public=Public
-publish=Publish
+publish-link-generate=Generate Read-Only Link
+publish-link-revoke=Revoke Read-Only Link
+publish=Share Read-Only Link
 rate-firetext=Rate Firetext
 raw=Raw
 recent-documents=Recently opened documents
@@ -132,6 +134,7 @@ save=Save
 select-all=Select all
 select-language=Select your language
 settings=Settings
+share=Share
 shared-document=Shared Document
 shared-sdcard=The SDCard on your device is shared, and thus not available. Try disabling USB Mass Storage in your settings.
 size=Size
@@ -146,7 +149,7 @@ underline=Underline
 undo=Undo
 unknown-error=Unknown error
 use-dropbox=Use Dropbox
-use-password=Only with Password
+use-password=Protect with Password
 valid-integer-value=Please enter a valid value (e.g. 2 or 5)
 visual=Visual
 want-to-delete-plural=Do you want to delete these files?

locales/nl.app.properties
@@ -13,10 +13,9 @@ textColor=Tekstkleur
 highlightColor=Markeerkleur
 cancel=Annuleren
 cant-collab-nohist=Je kan alleen samenwerken met een van de "Worker" abonnementen.
-cant-collab-noninternal=Je kan alleen samenwerken aan documenten op de interne opslag.
 cant-collab-too-old=Je versie van Airborn OS is te oud om samen te werken aan dit document.
-cant-publish-demo=Je kan vanuit de Demoversie geen documenten publiceren.
-cant-publish-noninternal=Je kan alleen documenten op de interne opslag publiceren.
+cant-publish-demo=Je kan vanuit de Demoversie geen alleen-lezen link delen.
+cant-share-noninternal=Je kan alleen documenten op de interne opslag delen.
 cant-view-hist-noninternal=Je kan alleen van documenten op de interne opslag geschiedenis bekijken.
 cant-view-hist=Je kan alleen geschiedenis bekijken met een van de "Worker" abonnementen.
 center=Midden
@@ -30,6 +29,8 @@ click-to-open-plans=Klik hier om abonnementen te bekijken.
 click-to-update=Klik hier om Airborn OS te updaten.
 close=Sluiten
 cloud=Cloud
+collab-link-generate=Samenwerklink genereren
+collab-link-revoke=Samenwerklink intrekken
 collaborate=Samenwerken
 contains-special-characters=Speciale tekens zijn niet toegestaan, gelieve dit te herzien.
 content-not-defined=De inhoud was niet gedefinieerd.
@@ -118,8 +119,9 @@ portrait=Staand
 print-view=Afdrukopmaak
 print=Afdrukken
 privacy=Privacy
-public=Openbaar
-publish=Publiceren
+publish-link-generate=Alleen-lezen link genereren
+publish-link-revoke=Alleen-lezen link intrekken
+publish=Alleen-lezen link delen
 rate-firetext=Firetext beoordelen
 raw=Onbewerkt
 recent-documents=Recent geopende documenten
@@ -133,6 +135,7 @@ save=Opslaan
 select-all=Alles selecteren
 select-language=Selecteer uw taal
 settings=Instellingen
+share=Delen
 shared-document=Gedeeld document
 shared-sdcard=De SD-kaart van uw apparaat is niet beschikbaar omdat deze gedeeld is. Probeer USB-opslag uit te schakelen in de instellingen.
 size=Formaat
@@ -146,7 +149,7 @@ underline=Onderstrepen
 undo=Ongedaan maken
 unknown-error=Onbekende fout
 use-dropbox=Dropbox gebruiken
-use-password=Alleen met wachtwoord
+use-password=Bescherm met wachtwoord
 valid-integer-value=Voer een geldige waarde in (bijv. 2 of 5)
 visual=Visueel
 want-to-delete-plural=Wilt u deze bestanden verwijderen?

scripts/firetext.js
@@ -1548,14 +1548,14 @@ function processActions(eventAttribute, target, event) {
 					margin: margin,
 				}
 			});
-		} else if (calledFunction == 'openCollab') {
+		} else if (calledFunction == 'openShare') {
 			var location = document.getElementById('currentFileLocation').textContent;
 			var directory = document.getElementById('currentFileDirectory').textContent;
 			var filename = document.getElementById('currentFileName').textContent;
 			var filetype = document.getElementById('currentFileType').textContent;
 			
 			if(location.substr(0, 8) !== 'internal') {
-				firetext.notify(navigator.mozL10n.get('cant-collab-noninternal'));
+				firetext.notify(navigator.mozL10n.get('cant-share-noninternal'));
 				return;
 			}
 			if (directory[0] !== '/') {
@@ -1563,25 +1563,63 @@ function processActions(eventAttribute, target, event) {
 			}
 			ljs.load('sjcl', function() {
 				var shared = firetext.shared.get(location + directory + filename + filetype);
-				if(shared && shared['collab-ACL']) {
-					document.getElementById('collab-ACL').value = shared['collab-ACL'];
+				var collab = shared['collab-ACL'] && shared['collab-ACL'] !== 'private';
+				var publish = shared['publish-ACL'] && shared['publish-ACL'] !== 'private';
+				if(collab) {
 					document.getElementById('collab-use-password').checked = shared['collab-use-password'];
 					document.getElementById('collab-password').value = shared['collab-password'];
 					document.getElementById('collab-iter').value = shared['collab-iter'];
 					document.getElementById('collab-salt').value = shared['collab-salt'];
 					document.getElementById('collab-link').value = firetext.shared.getCollabLink(shared);
 				} else {
-					document.getElementById('collab-ACL').value = 'public-read-write';
 					document.getElementById('collab-use-password').checked = false;
-					document.getElementById('collab-password').value = generateNewPassword();
+					document.getElementById('collab-password').value = '';
 					document.getElementById('collab-iter').value = 10000;
 					document.getElementById('collab-salt').value = sjcl.codec.base64.fromBits(sjcl.random.randomWords(2,0));
 					document.getElementById('collab-link').value = '';
 				}
+				document.getElementById('collab-use-password').disabled = collab;
+				document.getElementById('collab-password').readOnly = collab;
+				document.getElementById('collab-link-generate').style.display = collab ? 'none' : '';
+				document.getElementById('collab-link-revoke').style.display = collab ? '' : 'none';
+				document.getElementById('publish-link-generate').disabled =
+				document.getElementById('publish-link-revoke').disabled = collab;
+				if(publish) {
+					document.getElementById('publish-use-password').checked = shared['publish-use-password'];
+					document.getElementById('publish-password').value = shared['publish-password'];
+					document.getElementById('publish-iter').value = shared['publish-iter'];
+					document.getElementById('publish-salt').value = shared['publish-salt'];
+					document.getElementById('publish-link').value = firetext.shared.getPublishLink(shared);
+				} else {
+					document.getElementById('publish-use-password').checked = false;
+					document.getElementById('publish-password').value = '';
+					document.getElementById('publish-iter').value = 10000;
+					document.getElementById('publish-salt').value = sjcl.codec.base64.fromBits(sjcl.random.randomWords(2,0));
+					document.getElementById('publish-link').value = '';
+				}
+				document.getElementById('publish-use-password').disabled = publish;
+				document.getElementById('publish-password').readOnly = publish;
+				document.getElementById('publish-link-generate').style.display = publish ? 'none' : '';
+				document.getElementById('publish-link-revoke').style.display = publish ? '' : 'none';
 				
-				regions.nav('collab');
+				airborn.fs.getObjectLocation('/Documents/', function(ownObjectLoc) {
+					if(shared.S3Prefix && shared.S3Prefix !== ownObjectLoc.S3Prefix) {
+						document.getElementById('collab-link-generate').disabled =
+						document.getElementById('collab-link-revoke').disabled =
+						document.getElementById('collab-use-password').disabled =
+						document.getElementById('publish-link-generate').disabled =
+						document.getElementById('publish-link-revoke').disabled =
+						document.getElementById('publish-use-password').disabled = true;
+						document.getElementById('collab-password').readOnly =
+						document.getElementById('publish-password').readOnly = true;
+					}
+					
+					regions.nav('share');
+				});
 			});
 		} else if (calledFunction == 'collab') {
+			var revoke = target.hasAttribute(eventAttribute + '-revoke');
+			
 			var location = document.getElementById('currentFileLocation').textContent;
 			var directory = document.getElementById('currentFileDirectory').textContent;
 			var filename = document.getElementById('currentFileName').textContent;
@@ -1595,9 +1633,9 @@ function processActions(eventAttribute, target, event) {
 					firetext.notify(navigator.mozL10n.get('cant-collab-nohist'), '<a href="' + airborn.top_location.origin + '/plans" target="_blank">' + navigator.mozL10n.get('click-to-open-plans').replace(/</g, '&lt;') + '</a>', 10000);
 					return;
 				}
-				var shared = firetext.shared.get(location + directory + filename + filetype, shared) || {};
+				var shared = firetext.shared.get(location + directory + filename + filetype, shared);
 				for(var i in objectLoc) { shared[i] = objectLoc[i]; }
-				shared['collab-ACL'] = document.getElementById('collab-ACL').value;
+				shared['collab-ACL'] = revoke ? 'private' : 'public-read-write';
 				shared['collab-use-password'] = document.getElementById('collab-use-password').checked;
 				shared['collab-password'] = document.getElementById('collab-password').value;
 				shared['collab-iter'] = parseInt(document.getElementById('collab-iter').value);
@@ -1607,41 +1645,17 @@ function processActions(eventAttribute, target, event) {
 				firetext.shared.set(location + directory + filename + filetype, shared);
 				firetext.shared.updateCollab(location + directory + filename + filetype);
 				saveFromEditor(true, true);
-				document.getElementById('collab-link').value = firetext.shared.getCollabLink(shared);
-			});
-		} else if (calledFunction == 'openPublish') {
-			var location = document.getElementById('currentFileLocation').textContent;
-			var directory = document.getElementById('currentFileDirectory').textContent;
-			var filename = document.getElementById('currentFileName').textContent;
-			var filetype = document.getElementById('currentFileType').textContent;
-			
-			if(location.substr(0, 8) !== 'internal') {
-				firetext.notify(navigator.mozL10n.get('cant-publish-noninternal'));
-				return;
-			}
-			if (directory[0] !== '/') {
-				directory = '/sdcard/' + directory;
-			}
-			ljs.load('sjcl', function() {
-				var shared = firetext.shared.get(location + directory + filename + filetype);
-				if(shared && shared['publish-ACL']) {
-					document.getElementById('publish-ACL').value = shared['publish-ACL'];
-					document.getElementById('publish-use-password').checked = shared['publish-use-password'];
-					document.getElementById('publish-password').value = shared['publish-password'];
-					document.getElementById('publish-iter').value = shared['publish-iter'];
-					document.getElementById('publish-salt').value = shared['publish-salt'];
-					document.getElementById('publish-link').value = firetext.shared.getPublishLink(shared);
-				} else {
-					document.getElementById('publish-ACL').value = 'public-read';
-					document.getElementById('publish-use-password').checked = false;
-					document.getElementById('publish-iter').value = 10000;
-					document.getElementById('publish-salt').value = sjcl.codec.base64.fromBits(sjcl.random.randomWords(2,0));
-					document.getElementById('publish-link').value = '';
-				}
-				
-				regions.nav('publish');
+				document.getElementById('collab-link').value = revoke ? '' : firetext.shared.getCollabLink(shared);
+				document.getElementById('collab-use-password').disabled = !revoke;
+				document.getElementById('collab-password').readOnly = !revoke;
+				document.getElementById('collab-link-generate').style.display = !revoke ? 'none' : '';
+				document.getElementById('collab-link-revoke').style.display = !revoke ? '' : 'none';
+				document.getElementById('publish-link-generate').disabled =
+				document.getElementById('publish-link-revoke').disabled = !revoke;
 			});
 		} else if (calledFunction == 'publish') {
+			var revoke = target.hasAttribute(eventAttribute + '-revoke');
+			
 			var location = document.getElementById('currentFileLocation').textContent;
 			var directory = document.getElementById('currentFileDirectory').textContent;
 			var filename = document.getElementById('currentFileName').textContent;
@@ -1655,9 +1669,9 @@ function processActions(eventAttribute, target, event) {
 					firetext.notify(navigator.mozL10n.get('cant-publish-demo'));
 					return;
 				}
-				var shared = firetext.shared.get(location + directory + filename + filetype, shared) || {};
+				var shared = firetext.shared.get(location + directory + filename + filetype, shared);
 				for(var i in objectLoc) { shared[i] = objectLoc[i]; }
-				shared['publish-ACL'] = document.getElementById('publish-ACL').value;
+				shared['publish-ACL'] = revoke ? 'private' : 'public-read';
 				shared['publish-use-password'] = document.getElementById('publish-use-password').checked;
 				shared['publish-password'] = document.getElementById('publish-password').value;
 				shared['publish-iter'] = parseInt(document.getElementById('publish-iter').value);
@@ -1665,7 +1679,11 @@ function processActions(eventAttribute, target, event) {
 				shared.path = location + directory + filename + filetype;
 				firetext.shared.set(location + directory + filename + filetype, shared);
 				saveFromEditor(true, true);
-				document.getElementById('publish-link').value = firetext.shared.getPublishLink(shared);
+				document.getElementById('publish-link').value = revoke ? '' : firetext.shared.getPublishLink(shared);
+				document.getElementById('publish-use-password').disabled = !revoke;
+				document.getElementById('publish-password').readOnly = !revoke;
+				document.getElementById('publish-link-generate').style.display = !revoke ? 'none' : '';
+				document.getElementById('publish-link-revoke').style.display = !revoke ? '' : 'none';
 			});
 		} else if (calledFunction == 'openHistory') {
 			if(document.querySelector('.current').id == 'sidebar_history') {

scripts/regions.js
@@ -95,7 +95,7 @@ regions.nav = function (location, histEntry) {
 		} else if (location != 'welcome-edit-mode') {
 			// Focus first input
 			setTimeout(function() {
-				var input = locationElement.querySelector('input:not([disabled]), .config-dialog select') || locationElement.querySelector('button');
+				var input = locationElement.querySelector('input:not([disabled]), .config-dialog select, .config-dialog table button') || locationElement.querySelector('button');
 				if (input) {
 					editor.blur();
 					input.focus();

style/bb/input_areas.css
@@ -50,8 +50,8 @@ textarea:invalid {
   -moz-appearance: textfield;
 }
 
-.form p input + button[type="reset"],
-.form p textarea + button[type="reset"] {
+.form p input ~ button[type="reset"],
+.form p textarea ~ button[type="reset"] {
   position: absolute;
   top: 0;
   right: 0;
@@ -67,7 +67,7 @@ textarea:invalid {
   transition: opacity .3s;
 }
 
-.form p input[type="number"] + button[type="reset"] {
+.form p input[type="number"] ~ button[type="reset"] {
   transition-duration: 0s;
 }
 
@@ -85,10 +85,10 @@ textarea {
   padding-right: 3rem;
 }
 
-.form p:hover input + button[type="reset"],
-.form p input:focus + button[type="reset"],
-.form p:hover textarea + button[type="reset"],
-.form p textarea:focus + button[type="reset"] {
+.form p:hover input ~ button[type="reset"],
+.form p input:focus ~ button[type="reset"],
+.form p:hover textarea ~ button[type="reset"],
+.form p textarea:focus ~ button[type="reset"] {
   opacity: 1;
   pointer-events: all;
 }
@@ -284,8 +284,8 @@ section[role="region"] > header:first-child + .form[role="search"]:after {
   padding: 0.8rem 0.8rem 0.8rem 3.5rem;
 }
 
-[dir="rtl"] .form p input + button[type="reset"],
-[dir="rtl"] .form p textarea + button[type="reset"] {
+[dir="rtl"] .form p input ~ button[type="reset"],
+[dir="rtl"] .form p textarea ~ button[type="reset"] {
   right: auto;
   left: 0;
 }

style/main.css
@@ -511,9 +511,13 @@ aside select.dummy {
 }
 
 .config-dialog table td:first-child {
-  width: 190px;
+  width: 230px;
   height: 4.2rem;
-  padding-left: 1rem;
+  padding: 0.2rem 1rem;
+}
+
+.config-dialog table button {
+  margin: 0;
 }
 
 .config-dialog table select {
@@ -523,17 +527,32 @@ aside select.dummy {
 
 .config-dialog table input {
   margin: 0;
+  vertical-align: middle;
+}
+
+.config-dialog table label {
+  padding-top: .4rem;
+  display: inline-block;
 }
 
 .config-dialog table span {
-  vertical-align: top;
   margin-right: 2rem;
+}
+
+.config-dialog table input[disabled] + span,
+.config-dialog table .share-password-input[readonly] {
+  color: rgb(100, 100, 100);
 }
 
 .config-dialog .input-icon-button {
   background: none !important;
   font-size: 1.6rem !important;
   transform: scaleX(-1);
+}
+
+.config-dialog input:not([readonly]) ~ .copy-button,
+.config-dialog input[readonly] ~ .generate-button {
+  display: none;
 }
 
 /* Spinner (Throbber) */

commit 9f58438930afa997ff68f7e494fb2636a6aec06d

Fix auto-opening shared documents
Broken in 2a7cc480.

scripts/shared.js
@@ -8,8 +8,11 @@ firetext.shared = {
 			return firetext.io.split(path.substr(path.indexOf('/'))).concat('', path.substr(0, path.indexOf('/')));
 		});
 	},
+	getRaw: function(path) {
+		return JSON.parse(localStorage['firetext.shared'] || '{}')[path];
+	},
 	get: function(path) {
-		return JSON.parse(localStorage['firetext.shared'] || '{}')[path] || {};
+		return this.getRaw(path) || {};
 	},
 	getOptions: function(path, callback) {
 		var attrs = this.get(path);
@@ -161,7 +164,7 @@ function openSharedDocument(password, dontNotify) {
 		throw new Error('Document is collab-version ' + props['collab-version'] + ', this Firetext is collab-version ' + firetext.shared.collabVersion);
 	}
 	var path = props.path.replace('internal/', 'internal-' + props.S3Prefix + '/');
-	var newDoc = !firetext.shared.get(path);
+	var newDoc = !firetext.shared.getRaw(path);
 	firetext.shared.set(path, props);
 	if(newDoc) {
 		setTimeout(function() {